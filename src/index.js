d769859c1740e392907e64eeae4fa8fdbff1a819
Scott Lowe <scott@entercastle.com>
2015-05-15 23:19:49 -0400

    feat(ascii art): add Castle logo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-module.exports = {};
+var chalk = require('chalk');
+
+module.exports = {
+  printLogo: function () {
+    var art = [
+      '                                                            ',
+      '                                                            ',
+      '                           ' + chalk.green('======') + '                           ',
+      '                        ' + chalk.green('+===========') + '                        ',
+      '                      ' + chalk.green('================,') + '                     ',
+      '                  ' + chalk.green('.==========') + chalk.bgWhite('77') + chalk.green('==========') + '                   ',
+      '                ' + chalk.green('===========') + chalk.bgWhite('777777') + chalk.green('===========') + '                ',
+      '              ' + chalk.green('=============') + chalk.bgWhite('777777') + chalk.green('==============') + '             ',
+      '              ' + chalk.green('=======') + chalk.bgWhite('77') + chalk.green('====') + chalk.bgWhite('777777') + chalk.green('====') + chalk.bgWhite('77') + chalk.green('=======+') + '             ',
+      '              ' + chalk.green('=====') + chalk.bgWhite('7777') + chalk.green('==') + chalk.bgWhite('7777777777') + chalk.green('==') + chalk.bgWhite('7777') + chalk.green('+====+') + '             ',
+      '              ' + chalk.green('=====') + chalk.bgWhite('7777777777777777777777') + chalk.green('+====+') + '             ',
+      '              ' + chalk.green('=====') + chalk.bgWhite('7777777777777777777777') + chalk.green('+====+') + '             ',
+      '              ' + chalk.green('=====') + chalk.bgWhite('7777777777777777777777') + chalk.green('+====+') + '             ',
+      '              ' + chalk.green('=====') + chalk.bgWhite('7777777777777777777777') + chalk.green('+====+') + '             ',
+      '              ' + chalk.green('=====') + chalk.bgWhite('777777777') + chalk.green('====') + chalk.bgWhite('777777777') + chalk.green('+=====') + '             ',
+      '              ' + chalk.green('=====') + chalk.bgWhite('777777') + chalk.green('+=========') + chalk.bgWhite('777777') + chalk.green('+====+') +'             ',
+      '              ' + chalk.green('=====') + chalk.bgWhite('7777') + chalk.green('==============') + chalk.bgWhite('7777') + chalk.green('+====+') + '             ',
+      '              ' + chalk.green('=====') + chalk.bgWhite('777') + chalk.green('================') + chalk.bgWhite('I77') + chalk.green('+=====') + '             ',
+      '              ' + chalk.green('=======') + chalk.bgWhite('777?') + chalk.green('===========') + chalk.bgWhite('777?') + chalk.green('======+') + '             ',
+      '              ' + chalk.green('=========') + chalk.bgWhite('7777') + chalk.green('======') + chalk.bgWhite('7777') + chalk.green('==========') + '             ',
+      '                ' + chalk.green('==========') + chalk.bgWhite('77777777') + chalk.green('==========') + '                ',
+      '                  ' + chalk.green(':==========') + chalk.bgWhite('77?') + chalk.green('==========') + '                  ',
+      '                     ' + chalk.green('+=================') + '                     ',
+      '                        ' + chalk.green('============') + '                        ',
+      '                           ' + chalk.green('======:') + '                          ',
+      '                             ' + chalk.green('==') + '                             ',
+      '                                                            ',
+      '                                                            ',
+      '                                                            ',
+      '    ' + chalk.blue(':::::::') + '     ' + chalk.blue('::') + '      ' + chalk.blue('::::::') + ' ' + chalk.blue(':::::::::') + '  ' + chalk.blue('::') + '      ' + chalk.blue(':::::::') + '   ',
+      '  ' + chalk.blue(':::') + '           ' + chalk.blue(':::') + '    ' + chalk.blue('::') + '          ' + chalk.blue('::') + '     ' + chalk.blue('::') + '      ' + chalk.blue('::') + '        ',
+      '  ' + chalk.blue('::') + '           ' + chalk.blue('::::') + '     ' + chalk.blue('::') + '         ' + chalk.blue('::') + '     ' + chalk.blue('::') + '      ' + chalk.blue('::') + '        ',
+      '  ' + chalk.blue('::') + '          ' + chalk.blue('::') + '  ' + chalk.blue('::') + '     ' + chalk.blue('::::') + '      ' + chalk.blue('::') + '     ' + chalk.blue('::') + '      ' + chalk.blue('::::::,') + '   ',
+      '  ' + chalk.blue('::') + '          ' + chalk.blue(':') + '    ' + chalk.blue('::') + '       ' + chalk.blue('::') + '     ' + chalk.blue('::') + '     ' + chalk.blue('::') + '      ' + chalk.blue('::') + '        ',
+      '   ' + chalk.blue('::') + '     ' + chalk.blue(':') + '  ' + chalk.blue('::') + '    ' + chalk.blue('::') + '  ' + chalk.blue( ':' ) + '    ' + chalk.blue('::') + '     ' + chalk.blue('::') + '     ' + chalk.blue('::') + '      ' + chalk.blue('::') + '        ',
+      '    ' + chalk.blue(':::::::') + ' ' + chalk.blue('::') + '      ' + chalk.blue('::') + ' ' + chalk.blue('::::::') + '      ' + chalk.blue('::') + '     ' + chalk.blue('::::::,') + ' ' + chalk.blue(':::::::') + '   ',
+      '                                                            ',
+      '                                                            '
+    ];
+    for (var line = 0; line < art.length; line++) {
+      console.log(art[line]);
+    }
+  }
+};
